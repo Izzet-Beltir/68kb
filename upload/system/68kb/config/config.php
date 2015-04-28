65284ee4349fa5f1c3081230bef899372cf8fb3f
Izzet-Beltir <github@n.0pe.de>
2015-04-28 13:31:26 +0200

    base_url() on Windows systems

--- upload/system/68kb/config/config.php
+++ upload/system/68kb/config/config.php
@@ -12,7 +12,7 @@
 |
 */
 $config['base_url'] = "http://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= preg_replace('@/+$@','',dirname($_SERVER['SCRIPT_NAME'])).'/';
+$config['base_url'] .= preg_replace('@/+$@','',str_replace('\\', '/', dirname($_SERVER['SCRIPT_NAME']))).'/';
 
 /*
 |--------------------------------------------------------------------------
